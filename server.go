a2f66d21947eadb66ca16672f07bb0500a696b77
Martin Villumsen <mvil@tv2.dk>
2025-04-15 10:45:57 +0200

    Check return type

--- server.go
+++ server.go
@@ -6,5 +6,7 @@ import (
 )
 
 func Server(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, World!")
+	if _, err := fmt.Fprint(w, "Hello, World!"); err != nil {
+		http.Error(w, "Failed to write response", http.StatusInternalServerError)
+	}
 }
